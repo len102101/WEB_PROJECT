3c3b4bc6e68122b63051c54d61327d94e269b5d3
Nakite0529 <skdus0186@naver.com>
2019-06-27 11:50:12 +0900

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import VueTheMask from 'vue-the-mask'
-// import VeeValidate from 'vee-validate'
-// import Meta from 'vue-meta'
 import http from 'axios'
-
-// Vue.config.productionTip = false
-// Vue.use(VueTheMask)
-// Vue.use(VeeValidate)
-// Vue.use(Meta)
 
 Vue.prototype.$http = http;
 Vue.config.productionTip = false
